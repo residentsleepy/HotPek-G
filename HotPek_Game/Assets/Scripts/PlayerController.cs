bbe9ae121234d2e50b806e0bb54f86dfee601143
Popocas96 <74360893+Popocas96@users.noreply.github.com>
2020-11-22 21:32:09 -0600

    Adding Movement
    Add GameTest Scene
    Add Package Cinemachine
    Player Main Controler

---
+++ HotPek_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //El jugador siempre avanzara a adelante con respecto a la ubicacion de la camara.
+    //Necesitamos el objeto camara para saber a donde esta apuntando
+    public Transform cam;
+    public float speed = 6f;
+    Rigidbody rb;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    private void Start()
+    {
+        //Ubicamos el RigidBody del personaje para poder accesar a el mas fácil.
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+  
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //leemos las entradas del jugador.
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //las guadarmos en un vector 3 para poder usarlas mas adelante
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //verificamos si esta en moviendo el control, y entramos al ciclo
+        if (direction.magnitude >= 0.1f)
+        {
+            //Obtenemos los grados que giro el control para posicionar a nuestro jugador de frente 
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)* Mathf.Rad2Deg + cam.eulerAngles.y;
+            //Hacemos que el giro sea suave y no salte de frame a frame.
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            //giramos a nuestro personaje para que siempre "viendo a dalante" 
+            transform.localRotation = Quaternion.Euler(0f,angle,0f);
+            //Anhadimos la fuerza de movimiento.
+            rb.AddForce(transform.forward * speed);
+          
+            
+        }
+    }
+}
