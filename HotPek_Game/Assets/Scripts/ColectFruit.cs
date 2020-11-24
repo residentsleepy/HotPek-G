2d7366ffa972ab805be54a37b4a00a7039ae3d3b
RoliCorrea <rolicorrea@live.com.mx>
2020-11-24 15:10:58 -0600

    Cambios de idioma
    Dialogue y ColectFruit fueron actualizados para tener las anotaciones en inglés

--- HotPek_Game/Assets/Scripts/ColectFruit.cs
+++ HotPek_Game/Assets/Scripts/ColectFruit.cs
@@ -2,76 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//ESTE CODIGO VA EN EL PERSONAJE
-//ASEGURAR QUE TENGA EL BOX COLLIDER PARA INTERACTUAR
+
+//THIS CODE BELONGS TO THE CHARACTER, IT NEEDS A BOX COLLIDER TO BE ABLE TO WORK
+
+//This code allow us to check if the character is able to pick up fruit and increase it's score.
+//It can be used also to interact with other object by using the Box Collider the character has infront of it
 
 public class ColectFruit : MonoBehaviour
 {
-    //Collider que se usa para identificar cuando estamos con un objeto interactuable
+    //This represents the collider in front of our character, used for detect interactable objects.
     private BoxCollider colli;
-    //Puntuación del jugador
+    //Player's score and how much will it increase
     private int score;
-    //Part solo sirve para la instancia de las partículas, fruit es para identificar el objeto que será "recogido"
+    public int scoreBonus = 10;
+    //Part is just a placeholder we need, fruit represents the fruit or object we interact with
     private GameObject part, fruit;
-    //Sistema de partículas, se define en el Inspector
+    //This allow us to use a particle system for more effects in the game. We manually control it in the Inspector.
     public GameObject particle;
-    //Se guarda la posición de la fruta que causó el ultimo trigger
+    //We save the position of the last object to cause a trigger
     private Vector3 fruitPos;
-    //Indica si se puede tomar la fruta
+    //With this boolean we set if it is posible to collect an object
     private bool collectable = false;
    
+
     void Start()
     {
-        //Conseguimos el Collider que usaremos para detectar frutas
+        //We set our Box Collider to be used
         colli = GetComponent<BoxCollider>();
-        //Establecemos la puntuación inicial
+        //And we stablish our initial score
         score = 0;
+        //This line of code is used to check if the score is correct with each interaction
         Debug.Log("Score: " + score);
     }
 
     void Update()
     {
-        //Si apretamos espacio mientras exista la posibilidad de recoger...
+        //If we are able to collect we can press the left click to trigger some events:
         if (Input.GetKeyDown(KeyCode.Space) && collectable == true)
         {
             Debug.Log("Space Pressed");
-            //Se intancia las particulas
+            //The particle system we are using is triggered
             part = Instantiate(particle, fruitPos, Quaternion.identity);
-            //La puntuación sube
-            score += 10;
+            //Our score goes up
+            score += scoreBonus;
             Debug.Log("Score: " + score);
-            //La fruta desaparece y volvemos a un estado donde no se puede recoger nada aún
+            //And the fruit we collected disappears and our state goes back to being unable to collect
             Destroy(fruit);
             collectable = false;
         }
     }
 
-    //Si hacemos trigger de una colisión...
-    //(Las frutas deben tener trigger y Rigibody)
+    //If we have a trigger through our collider...
+    //(All interactable objects must have a trigger and Rigibody)
     private void OnTriggerEnter(Collider col)
     {
         Debug.Log("Trigger");
-        //Si es fruta...
+        //If we interact with a fruit
         if (col.gameObject.tag == "Fruit")
         {
             Debug.Log("Trigger with Fruit");
-            //Guardamos su posición
+            //We save its position
             fruitPos = col.gameObject.transform.position;
-            //Comprobación
+            //Quick check of data
             Debug.Log(col.gameObject.transform.position.x);
             Debug.Log(col.gameObject.transform.position.y);
             Debug.Log(col.gameObject.transform.position.z);
-            //Establecemos la variable para después poder borrar el objeto
+            //We set a variable to have the trigger fruit to be an active element we are interacting with
             fruit = col.gameObject;
-            //Podemos recoger
+            //And we set us to be able to collect
             collectable = true;            
         }
     }
 
-    //Si salimos de trigger...
+    //When we exit a trigger
     private void OnTriggerExit(Collider col)
     {
-        //No podemos recoger
+        //We need to establish that we can't collect
         Debug.Log("Trigger Exit");
         collectable = false;
     }
--- HotPek_Game/Assets/Scripts/Dialogue.cs
+++ HotPek_Game/Assets/Scripts/Dialogue.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//ESTE CODIGO VA EN EL OBJETO O PERSONAJE QUE TENGA DIALOGO
+//THIS CODE BELONGS TO A CHARACTER OR OBJECT THAT NEEDS DIALOGUE
 
+
+//With the code being serializable we can use it in the Inspector when other code requires it
 [System.Serializable]
 public class Dialogue
 {
+    //This can be used for the name of the character or object we interact with and has dialogue
     public string name;
-
+    //This serializable array is used for all the sentences created for the character or object
     [TextArea(3, 10)]
     public string[] sentences;
 
