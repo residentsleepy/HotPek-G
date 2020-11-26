2c3f7f6f1d55782d52fa3cd48349cfb224823b79
deletedman <deletedman2@gmail.com>
2020-11-26 14:44:05 -0500

    script"Enemy" añadido para perseguir al juador

---
+++ HotPek_Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent nm;
+    public Transform target;
+
+    public float distanceThreshold = 10f;
+
+    public enum AIState { idle,chasing};
+    public AIState aiState = AIState.idle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+        StartCoroutine(Think());
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Think()
+    {
+        while (true)
+        {
+            switch (aiState)
+            {
+                case AIState.idle:
+                    float dist = Vector3.Distance(target.position, transform.position);
+                    if (dist < distanceThreshold)
+                    {
+                        aiState = AIState.chasing;
+                    }
+                    nm.SetDestination(transform.position);
+                    break;
+                case AIState.chasing:
+                    dist = Vector3.Distance(target.position, transform.position);
+                    if (dist > distanceThreshold)
+                    {
+                        aiState = AIState.idle;
+                    }
+                    nm.SetDestination(target.position);
+                    break;
+                default:
+                    break;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ HotPek_Game/Assets/Scripts/Patrol.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float patrolSpeed = 0f;
+    public float changeTargetDistance = 0.1f;
+    public Transform[] patrolPoints;
+    int currentTarget = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveToTarget())
+        {
+            //Get Next Target
+            GetNextTarget();
+        }
+    }
+
+    private bool MoveToTarget()
+    {
+        Vector3 distanceVector = patrolPoints[currentTarget].position - transform.position;
+        if(distanceVector.magnitude < changeTargetDistance)
+        {
+            return true;
+        }
+
+        Vector3 velocityVector = distanceVector.normalized;
+        transform.position += velocityVector * patrolSpeed * Time.deltaTime;
+
+        return false;
+    }
+
+    private int GetNextTarget()
+    {
+        currentTarget++;
+        if(currentTarget >= patrolPoints.Length)
+        {
+            currentTarget = 0;
+        }
+        return currentTarget;
+    }
+}
