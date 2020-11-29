be0ce0b576b7868fde9a56c34184c00b883a130e
Popocas96 <74360893+Popocas96@users.noreply.github.com>
2020-11-29 11:58:32 -0600

    Junk Animation
    Idle animaiton
    walk Animation

---
+++ HotPek_Game/Assets/Scripts/AnimationStateControllerJunk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateControllerJunk : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool("IsWalking");
+        bool pressW = false;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if(horizontal != 0 || vertical != 0)
+        {
+            pressW = true;
+        }
+
+        if (!isWalking && pressW)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        if (isWalking && !pressW)
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+}
