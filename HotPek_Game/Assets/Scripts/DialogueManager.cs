0aa43d34858f641fbff4f90dddfa26590d5497ab
RoliCorrea <rolicorrea@live.com.mx>
2020-11-30 03:27:06 -0600

    Audio/Dialogos/Niveles(0/1)
    Se agregaron algunos audios
    Se reparo un codigo de dialogos
    Se avanzo casi por completo el nivel 0 y 1

--- HotPek_Game/Assets/Scripts/DialogueManager.cs
+++ HotPek_Game/Assets/Scripts/DialogueManager.cs
@@ -2,36 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    //ESTE CODIGO VA EN UN PERSONAJE U OBJETO QUE TENGA DIALOGO
+    //ESTE CODIGO VA EN EL OBJETO DIALOGUE MANAGER
 
     //Con este código controlamos las oraciones de una personaje u objeto, así como el momento en que aparecen y desaparecen en pantalla
 
-    public Dialogue dialogue; //Usamos está clase para determinar el nombre y dialogo(s) del objeto/personaje
-    public Text nameText; //Cuadro de Texto de la UI donde aparecerá el nombre del objeto/personaje
-    public Text dialogueText; //Cuadro de Texto de la UI donde aparecerán los dialogos
+    public TextMeshProUGUI nameText; //Cuadro de Texto de la UI donde aparecerá el nombre del objeto/personaje
+    public TextMeshProUGUI dialogueText; //Cuadro de Texto de la UI donde aparecerán los dialogos
     public Animator animator; //El Animator es usado para controlar cuando aparecen los cuadros de texto en pantalla
     private Queue<string> sentences; //Usamos esta queue para controlar como aparece el texto
-    private float charSpeed; //Usamos este float para guardar la velocidad original del jugador
-
 
     void Start()
     {
         sentences = new Queue<string>(); //Creamos la queue que nos servirá en las funciones de los dialogos
     }
 
-    //Cuando se causa un trigger en el objeto...
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player") //Primero necesitamos determinar si el trigger fue causado por el jugador
-        {
-            charSpeed = other.GetComponent<PlayerController>().speed; //Guardamos el valor original de la velocidad del jugador
-            other.GetComponent<PlayerController>().speed = 0f; //Después de guardarlo lo hacemos 0 para evitar que se mueva durante el dialogo
-            StartDialogue(dialogue); //Llamamos la función dialogo usando el que declaramos en nuestras variables (y que configuramos en el Inspector)
-        }
-    }
+
 
     //Esta función es la usada para mostrar nuestros dialogos en pantalla usando el Animator
     //Se usa el Dialogue como parametro para tener los nombres y oraciones definidos
@@ -44,10 +33,7 @@ public class DialogueManager : MonoBehaviour
         //Este ciclo es usado para guardar cada sentence que aparecerá
         foreach (string sentence in dialogue.sentences)
         {
-            //LINEA DESHABILITADA //Se usará si le ponemos sonido a los dialogos
-            /*
             FindObjectOfType<AudioManager>().Play("BoxSound");
-            */
             sentences.Enqueue(sentence);
         }
         DisplayNextSentence(); //Llamamos a esta función para mostrar la siguiente oración
@@ -60,14 +46,10 @@ public class DialogueManager : MonoBehaviour
         //Primero se determina si ya no hay más oraciones para mostrar
         if (sentences.Count == 0)
         {
-            //LINEA DESHABILITADA //Se usará si le ponemos sonido a los dialogos
-            /*
             FindObjectOfType<AudioManager>().Play("BoxSound");
-            */
             //En caso de no tener mas oraciones llamamos a la función que termina los dialogos
             EndDialogue();
             //Y le devolvemos al jugador su velocidad original
-            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().speed = charSpeed;
             return;
         }
         //LINEA DESHABILITADA //Se usará si le ponemos sonido a los dialogos
@@ -97,4 +79,10 @@ public class DialogueManager : MonoBehaviour
         animator.SetBool("IsOpen", false); //Se determinar el booleano del Animator como falso para guardarlo
     }
 
+
+    //Con este sencillo código devolvemos la velocidad al jugador
+    public void ReturnSpeed(float charSpeed)
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().speed = charSpeed;
+    }
 }
---
+++ HotPek_Game/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    //ESTE CÓDIGO VA EN EL PERSONAJE U OBJETO QUE TENGA ALGÚN DIALOGO
+
+    //Este codigo regula las interacciones del jugador con cualquier otro personaje u objeto que tenga dialogo, entregandole sus lineas
+    //al Dialogue Manager para que pueda mostrarlas en pantalla.
+
+    public Dialogue dialogue; //Usamos está clase para determinar el nombre y dialogo(s) del objeto/personaje
+    private float charSpeed; //Usamos este float para guardar la velocidad original del jugador
+    
+    //Con esto hacemos referencia al Dialogue Manager para realizar el trabajo con los dialogos
+    void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue); //Llamamos a la función de dialogo dentro del DialogueManager
+    }
+
+    //Cuando se causa un trigger en el objeto...
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") //Primero necesitamos determinar si el trigger fue causado por el jugador
+        {
+            charSpeed = other.GetComponent<PlayerController>().speed; //Guardamos el valor original de la velocidad del jugador
+            other.GetComponent<PlayerController>().speed = 0f; //Después de guardarlo lo hacemos 0 para evitar que se mueva durante el dialogo
+            TriggerDialogue(); //Llamamos la función para empezar el dialogo
+            FindObjectOfType<DialogueManager>().ReturnSpeed(charSpeed); //Le devolvemos la velocidad al jugador
+        }
+    }
+}
