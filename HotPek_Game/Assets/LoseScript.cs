6efb07b2353b15969053a5bc77b57055b881bd17
cesarvenom <cesarvenom11@gmail.com>
2020-11-30 18:39:18 -0600

    loseUI
    botones funcionales.

---
+++ HotPek_Game/Assets/LoseScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScript : MonoBehaviour
+{
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenuOut()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
